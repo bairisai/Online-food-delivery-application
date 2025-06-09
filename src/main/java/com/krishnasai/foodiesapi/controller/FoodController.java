80842b7f6edaa2821c47f39e85395bf396ff070c
krish <krishnasaibairi56140@gmail.com>
2025-06-09 11:44:12 +0530

    init commit

--- src/main/java/com/krishnasai/foodiesapi/controller/FoodController.java
+++ src/main/java/com/krishnasai/foodiesapi/controller/FoodController.java
@@ -7,12 +7,11 @@ import com.krishnasai.foodiesapi.io.FoodResponse;
 import com.krishnasai.foodiesapi.service.FoodService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/foods")
@@ -22,7 +21,7 @@ public class FoodController {
     private final FoodService foodService;
 
     @PostMapping
-    private FoodResponse addFood(@RequestPart("food") String foodString,
+    public FoodResponse addFood(@RequestPart("food") String foodString,
                                  @RequestPart("file")MultipartFile file) {
         ObjectMapper objectMapper = new ObjectMapper();
         FoodRequest request = null;
@@ -33,5 +32,21 @@ public class FoodController {
         }
         FoodResponse response = foodService.addFood(request, file);
         return response;
+    }
+
+    @GetMapping
+    public List<FoodResponse> readFoods() {
+        return foodService.readFoods();
+    }
+
+    @GetMapping("/{id}")
+    public FoodResponse readFood(@PathVariable String id) {
+        return foodService.readFood(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFood(@PathVariable String id) {
+        foodService.deleteFood(id);
     }
 }
--- src/main/java/com/krishnasai/foodiesapi/service/FoodService.java
+++ src/main/java/com/krishnasai/foodiesapi/service/FoodService.java
@@ -4,7 +4,13 @@ import com.krishnasai.foodiesapi.io.FoodRequest;
 import com.krishnasai.foodiesapi.io.FoodResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FoodService {
     String uploadFile(MultipartFile file);
     FoodResponse addFood(FoodRequest request, MultipartFile file);
+    List<FoodResponse> readFoods();
+    FoodResponse readFood(String id);
+    boolean deleteFile(String filename);
+    void deleteFood(String id);
 }
